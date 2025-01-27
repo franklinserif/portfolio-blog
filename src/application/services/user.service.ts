b1988e8ad591661d62fb74d415a385d8f9bd4d72
franklin rodriguez <franklinserif@gmail.com>
2025-01-26 23:46:28 -0400

    refactor(@application/services): :recycle: improve UserServices

---
+++ src/application/services/user.service.ts
@@ -0,0 +1,27 @@
+import { CreateUser } from '@domain/useCases/createUser';
+import { TypeORMUserRepository } from '@infrastructure/repositories/TypeORMUserRepository';
+
+export class UserService {
+    private createUser: CreateUser;
+
+    constructor() {
+        const userRepository = new TypeORMUserRepository();
+        this.createUser = new CreateUser(userRepository);
+    }
+
+    async create(
+        id: string,
+        firstName: string,
+        lastName: string,
+        email: string,
+        password: string
+    ) {
+        return this.createUser.execute(
+            id,
+            firstName,
+            lastName,
+            email,
+            password
+        );
+    }
+}
