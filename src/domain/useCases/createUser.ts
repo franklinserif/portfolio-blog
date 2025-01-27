97ab366f68e656f6498bd359b65f6e1ac09c1eaf
franklin rodriguez <franklinserif@gmail.com>
2025-01-26 23:45:49 -0400

    refactor(@domain/useCases): :recycle: improve createUser

---
+++ src/domain/useCases/createUser.ts
@@ -0,0 +1,17 @@
+import { User } from '@domain/entities/user';
+import { UserRepository } from '@domain/repositories/UserRepository';
+
+export class CreateUser {
+    constructor(private userRepository: UserRepository) {}
+
+    async execute(
+        id: string,
+        firstName: string,
+        lastName: string,
+        email: string,
+        password: string
+    ): Promise<User> {
+        const user = new User(id, firstName, lastName, email, password);
+        return this.userRepository.create(user);
+    }
+}
