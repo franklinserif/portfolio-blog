3cb650f76d0e84d28cca6984f196e12d761da4f2
franklin rodriguez <franklinserif@gmail.com>
2025-01-26 23:53:01 -0400

    refactor(user): :recycle: add space and remove createdAt and updatedAt

--- src/domain/entities/user.ts
+++ src/domain/entities/user.ts
@@ -1,27 +1,29 @@
 export class User {
     id: string;
+
     firstName: string;
+
     lastName: string;
+
     email: string;
+
     password: string;
-    createdAt: Date;
-    updatedAt: Date;
 
     constructor(
         id: string,
         firstName: string,
         lastName: string,
         email: string,
-        password: string,
-        createdAt: Date,
-        updatedAt: Date
+        password: string
     ) {
         this.id = id;
+
         this.firstName = firstName.toLowerCase().trim();
+
         this.lastName = lastName.toLowerCase().trim();
+
         this.email = email.toLowerCase().trim();
+
         this.password = password;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
     }
 }
