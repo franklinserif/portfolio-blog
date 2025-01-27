309ca6f638b884bb8b232ce3b0edf2d8bb4e413b
franklin rodriguez <franklinserif@gmail.com>
2025-01-26 20:20:09 -0400

    feat(infrastructure/entities): :sparkles: add TypeORM user entity

---
+++ src/infrastructure/entities/user.entity.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+    @PrimaryGeneratedColumn('uuid')
+    id!: string;
+
+    @Column('text')
+    firstName!: string;
+
+    @Column('text')
+    lastName!: string;
+}
