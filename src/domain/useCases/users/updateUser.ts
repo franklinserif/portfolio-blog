f4bb68559bd3cc0293128be9effa47c5b5e485bd
franklin rodriguez <franklinserif@gmail.com>
2025-01-28 17:51:01 -0400

    refactor(useCases/updateUser): :recycle: improve implementation using DTO

--- src/domain/useCases/users/updateUser.ts
+++ src/domain/useCases/users/updateUser.ts
@@ -1,23 +1,13 @@
+import { UpdateUserDto } from '@application/dtos/users/updateUser.dto';
 import { User } from '@domain/entities/user';
 import { UserRepository } from '@domain/repositories/user.repository';
 
-export class UpdateUserUseCase {
+export class UpdateUser {
     constructor(private userRepository: UserRepository) {}
 
-    async execute(
-        id: string,
-        firstName?: string,
-        lastName?: string,
-        email?: string,
-        password?: string
-    ): Promise<User> {
+    async execute(id: string, updateUserDto: UpdateUserDto): Promise<User> {
         const user = User.serializeUser(
-            await this.userRepository.update(id, {
-                firstName,
-                lastName,
-                email,
-                password
-            })
+            await this.userRepository.update(id, updateUserDto)
         );
 
         return user;
