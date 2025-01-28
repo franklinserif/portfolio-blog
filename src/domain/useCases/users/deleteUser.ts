3e74dd4d1f1a5ab0604b1c495bb1357b0b80c8d5
franklin rodriguez <franklinserif@gmail.com>
2025-01-28 17:52:24 -0400

    refactor(useCases/deleteUser): :recycle: change name of the class

--- src/domain/useCases/users/deleteUser.ts
+++ src/domain/useCases/users/deleteUser.ts
@@ -1,7 +1,7 @@
 import { UserRepository } from '@domain/repositories/user.repository';
 import { DeleteResult } from 'typeorm';
 
-export class DeleteUserUseCase {
+export class DeleteUser {
     constructor(private userRepository: UserRepository) {}
 
     async execute(id: string): Promise<DeleteResult> {
