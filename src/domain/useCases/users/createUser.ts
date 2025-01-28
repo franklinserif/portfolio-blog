2249004903d67a95dc08207f51c7f0cede8fe91a
franklin rodriguez <franklinserif@gmail.com>
2025-01-28 17:29:41 -0400

    refactor(useCases/createUser): :recycle: change class name to CreateUserUseCase

--- src/domain/useCases/users/createUser.ts
+++ src/domain/useCases/users/createUser.ts
@@ -1,7 +1,7 @@
 import { User } from '@domain/entities/user';
 import { UserRepository } from '@domain/repositories/user.repository';
 
-export class CreateUser {
+export class CreateUserUseCase {
     constructor(private userRepository: UserRepository) {}
 
     async execute(
