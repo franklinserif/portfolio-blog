94e0e256c0429053582884e688ebe942402d5195
franklin rodriguez <franklinserif@gmail.com>
2025-01-28 23:33:03 -0400

    refactor(useCases/createUser): :recycle: add loger and hash password

--- src/domain/useCases/users/createUser.ts
+++ src/domain/useCases/users/createUser.ts
@@ -1,19 +1,35 @@
 import { CreateUserDto } from '@application/dtos/users/createUser.dto';
 import { User } from '@domain/entities/user';
 import { UserRepository } from '@domain/repositories/user.repository';
+import { ILogger } from '@shared/interfaces/logs';
+import { Crypt } from '@shared/utils/crypt/crypt';
+import { Logger } from '@shared/utils/logger/logger';
 
 export class CreateUser {
+    private readonly logger: ILogger = new Logger(CreateUser.name);
+
     constructor(private userRepository: UserRepository) {}
 
     async execute(createUserDto: CreateUserDto): Promise<User> {
-        const user = new User(
-            createUserDto.id,
-            createUserDto.firstName,
-            createUserDto.lastName,
-            createUserDto.email,
-            createUserDto.password
-        );
+        try {
+            const hashedPassword = await Crypt.hashPassword(
+                createUserDto.password
+            );
 
-        return this.userRepository.create(user);
+            const user = new User(
+                createUserDto.id,
+                createUserDto.firstName,
+                createUserDto.lastName,
+                createUserDto.email,
+                hashedPassword
+            );
+
+            return this.userRepository.create(user);
+        } catch (error) {
+            this.logger.error(
+                `something went wrong creating the user ${error}`
+            );
+            throw new Error(`something went wrong creating the user ${error}`);
+        }
     }
 }
