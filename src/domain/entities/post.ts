46d0747a1410757c65221305925e6d4a9e1cc846
franklin rodriguez <franklinserif@gmail.com>
2025-02-15 15:18:55 -0400

    refactor(post): :recycle: improve implementation

--- src/domain/entities/post.ts
+++ src/domain/entities/post.ts
@@ -1,21 +1,30 @@
 import { Post as TypeOrmPost } from '@infrastructure/entities/post.entity';
+import { User } from '@domain/entities/user';
+import { Tag } from '@domain/entities/tag';
+import { Comment } from '@domain/entities/comment';
 
 export class Post {
     id: string;
     title: string;
     content: string;
+    user: User;
+    tags?: Array<Tag>;
+    comments?: Array<Comment>;
 
-    constructor({ id, title, content }: Post) {
+    constructor({ id, title, content, user }: Post) {
         this.id = id;
         this.title = title;
         this.content = content;
+        this.user = user;
     }
 
     static serializePost(typeOrmPost: TypeOrmPost): Post {
         const post = new Post({
             id: typeOrmPost.id,
             title: typeOrmPost.title,
-            content: typeOrmPost.content
+            content: typeOrmPost.content,
+            user: typeOrmPost.user,
+            tags: typeOrmPost.tags
         });
 
         return post;
