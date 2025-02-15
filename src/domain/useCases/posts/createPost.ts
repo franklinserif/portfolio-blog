60210bb61f603acec45c6686fac48a3b6c466fc0
franklin rodriguez <franklinserif@gmail.com>
2025-02-15 17:29:26 -0400

    refactor(createPost): :recycle: improve implementation

--- src/domain/useCases/posts/createPost.ts
+++ src/domain/useCases/posts/createPost.ts
@@ -4,17 +4,30 @@ import { Post as TypeORMPost } from '@infrastructure/entities/post.entity';
 import { PostRepository } from '@domain/repositories/post.repository';
 import { ILogger } from '@shared/interfaces/logs';
 import { Logger } from '@shared/utils/logger/logger';
+import { UserRepository } from '@domain/repositories/user.repository';
 
 export class CreatePost {
     private readonly logger: ILogger = new Logger(CreatePost.name);
 
-    constructor(private postRepository: PostRepository) {}
+    constructor(
+        private readonly postRepository: PostRepository,
+        private readonly userRepository: UserRepository
+    ) {}
 
     async execute(createPostDto: CreatePostDto): Promise<Post> {
         try {
+            const user = await this.userRepository.findOne(
+                createPostDto.userId
+            );
+
             const post = new Post({
-                ...createPostDto,
-                comments: []
+                id: createPostDto.id,
+                title: createPostDto.title,
+                content: createPostDto.content,
+                urlPath: createPostDto.urlPath,
+                user,
+                comments: [],
+                tags: []
             }) as TypeORMPost;
 
             return this.postRepository.create(post);
