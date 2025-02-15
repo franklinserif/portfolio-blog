532b98af1fc41673bdae9854a9b2e8e61c848ec3
franklin rodriguez <franklinserif@gmail.com>
2025-02-15 15:14:22 -0400

    refactor(post.repository): :recycle: improve types

--- src/domain/repositories/post.repository.ts
+++ src/domain/repositories/post.repository.ts
@@ -1,15 +1,15 @@
 import { Post } from '@infrastructure/entities/post.entity';
 
+type TPost = Omit<Post, 'checkFieldsBeforeInsert' | 'createdAt' | 'updatedAt'>;
+
 export interface PostRepository {
     findAll(): Promise<Post[]>;
 
     findOne(id: string): Promise<Post>;
 
-    create(
-        post: Omit<Post, 'checkFieldsBeforeInsert' | 'createdAt' | 'updatedAt'>
-    ): Promise<Post>;
+    create(post: TPost): Promise<Post>;
 
-    update(id: string, post: Partial<Post>): Promise<Post>;
+    update(id: string, post: Partial<TPost>): Promise<Post>;
 
     remove(id: string): Promise<void>;
 }
