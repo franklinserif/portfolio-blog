7215c55da6b4cf452f2c27bb5715b7d970cc1f4d
franklin rodriguez <franklinserif@gmail.com>
2025-02-15 15:15:56 -0400

    refactor(user.repository): :recycle: improve types

--- src/domain/repositories/user.repository.ts
+++ src/domain/repositories/user.repository.ts
@@ -1,13 +1,18 @@
 import { User } from '@infrastructure/entities/user.entity';
 
+type TUser = Omit<
+    User,
+    'checkFieldsBeforeInsert' | 'updatedAt' | 'createdAt' | 'password'
+>;
+
 export interface UserRepository {
     findOne(id: string): Promise<User>;
 
     findAll(): Promise<User[]>;
 
-    create(user: Omit<User, 'checkFieldsBeforeInsert'>): Promise<User>;
+    create(user: TUser): Promise<User>;
 
-    update(id: string, user: Partial<User>): Promise<User>;
+    update(id: string, user: Partial<TUser>): Promise<User>;
 
     remove(id: string): Promise<void>;
 }
