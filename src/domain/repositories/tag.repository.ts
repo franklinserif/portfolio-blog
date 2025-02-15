35d26d5ef6223ffbb8580e0ad21996c9330acc0d
franklin rodriguez <franklinserif@gmail.com>
2025-02-15 15:14:39 -0400

    refactor(tag.repository): :recycle: improve types

--- src/domain/repositories/tag.repository.ts
+++ src/domain/repositories/tag.repository.ts
@@ -1,13 +1,15 @@
 import { Tag } from '@infrastructure/entities/tag.entity';
 
+type TTag = Omit<Tag, 'checkFieldsBeforeInsert' | 'updatedAt' | 'createdAt'>;
+
 export interface TagRepository {
     findAll(): Promise<Tag[]>;
 
     findOne(id: string): Promise<Tag>;
 
-    create(tag: Omit<Tag, 'checkFieldsBeforeInsert'>): Promise<Tag>;
+    create(tag: TTag): Promise<Tag>;
 
-    update(id: string, tag: Partial<Tag>): Promise<Tag>;
+    update(id: string, tag: Partial<TTag>): Promise<Tag>;
 
     remove(id: string): Promise<void>;
 }
