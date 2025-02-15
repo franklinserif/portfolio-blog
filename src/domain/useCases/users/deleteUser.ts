94c3969de010469e26e86f5c17d7172754c33716
franklin rodriguez <franklinserif@gmail.com>
2025-02-15 15:20:39 -0400

    refactor(deleteUser): :fire: remove DeleteResult and add Promise<void>

--- src/domain/useCases/users/deleteUser.ts
+++ src/domain/useCases/users/deleteUser.ts
@@ -1,10 +1,9 @@
 import { UserRepository } from '@domain/repositories/user.repository';
-import { DeleteResult } from 'typeorm';
 
 export class DeleteUser {
     constructor(private userRepository: UserRepository) {}
 
-    async execute(id: string): Promise<DeleteResult> {
-        return await this.userRepository.remove(id);
+    async execute(id: string): Promise<void> {
+        await this.userRepository.remove(id);
     }
 }
