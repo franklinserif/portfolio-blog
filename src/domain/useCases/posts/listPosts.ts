3144914254fa0295f9739d8b72b83e6d72dd7fed
franklin rodriguez <franklinserif@gmail.com>
2025-02-16 20:38:24 -0400

    refactor(useCases/listPosts): :recycle: improve implementation

--- src/domain/useCases/posts/listPosts.ts
+++ src/domain/useCases/posts/listPosts.ts
@@ -5,8 +5,14 @@ export class ListPosts {
     constructor(private readonly postRepository: PostRepository) {}
 
     async execute(): Promise<Post[]> {
-        const posts = Post.serializeAll(await this.postRepository.findAll());
+        try {
+            const posts = Post.serializeAll(
+                await this.postRepository.findAll()
+            );
 
-        return posts;
+            return posts;
+        } catch (error) {
+            throw new Error(`${error}`);
+        }
     }
 }
