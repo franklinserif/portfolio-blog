77bb9b39fbb0656e35da1d4c97c471fa8bd2b8c4
franklin rodriguez <franklinserif@gmail.com>
2025-02-17 15:06:05 -0400

    feat(post.router): :recycle: add validation

--- src/presentation/routes/post.router.ts
+++ src/presentation/routes/post.router.ts
@@ -1,6 +1,9 @@
 import { autoInjectable } from 'tsyringe';
 import { BaseRouter } from '@presentation/routes/router';
 import { PostController } from '@presentation/controllers/post.controller';
+import { validationMiddleware } from '@shared/middlewares/validation.middleware';
+import { CreatePostDto } from '@application/dtos/posts/createPost';
+import { UpdatePostDto } from '@application/dtos/posts/updatePost';
 
 @autoInjectable()
 export class PostRouter extends BaseRouter {
@@ -16,16 +19,20 @@ export class PostRouter extends BaseRouter {
             this.postController.findAllPosts(req, res)
         );
 
-        this.router.post('/posts', (req, res) =>
-            this.postController.createPost(req, res)
+        this.router.post(
+            '/posts',
+            validationMiddleware(CreatePostDto),
+            (req, res) => this.postController.createPost(req, res)
         );
 
         this.router.get('/posts/:id', (req, res) =>
             this.postController.findPost(req, res)
         );
 
-        this.router.put('/posts/:id', (req, res) =>
-            this.postController.updatePost(req, res)
+        this.router.put(
+            '/posts/:id',
+            validationMiddleware(UpdatePostDto),
+            (req, res) => this.postController.updatePost(req, res)
         );
 
         this.router.delete('/posts/:id', (req, res) =>
