4906b5722fe6f580421962acc30deb0c2d4d73c9
franklin rodriguez <franklinserif@gmail.com>
2025-02-16 20:35:15 -0400

    refactor(useCases/updateCase): :recycle: improve implementation

--- src/domain/useCases/users/updateUser.ts
+++ src/domain/useCases/users/updateUser.ts
@@ -1,15 +1,27 @@
 import { UpdateUserDto } from '@application/dtos/users/updateUser.dto';
 import { User } from '@domain/entities/user';
 import { UserRepository } from '@domain/repositories/user.repository';
+import { ILogger } from '@shared/interfaces/logs';
+import { Logger } from '@shared/utils/logger/logger';
 
 export class UpdateUser {
+    private readonly logger: ILogger = new Logger(UpdateUser.name);
+
     constructor(private userRepository: UserRepository) {}
 
     async execute(id: string, updateUserDto: UpdateUserDto): Promise<User> {
-        const user = User.serializeUser(
-            await this.userRepository.update(id, updateUserDto)
-        );
+        try {
+            const user = User.serializeUser(
+                await this.userRepository.update(id, updateUserDto)
+            );
 
-        return user;
+            this.logger.info(`user with id ${id} was updated`);
+
+            return user;
+        } catch (error) {
+            throw new Error(
+                `something wrong happend when tried to updated an user error: ${error}`
+            );
+        }
     }
 }
