41f60cf7d5a07b40823681777c5c916b0cb02fe9
franklin rodriguez <franklinserif@gmail.com>
2025-02-16 20:38:04 -0400

    refactor(useCases/updatePost): :recycle: improve implementation

--- src/domain/useCases/posts/updatePost.ts
+++ src/domain/useCases/posts/updatePost.ts
@@ -4,8 +4,12 @@ import { CommentRepository } from '@domain/repositories/comment.repository';
 import { PostRepository } from '@domain/repositories/post.repository';
 import { TagRepository } from '@domain/repositories/tag.repository';
 import { UserRepository } from '@domain/repositories/user.repository';
+import { ILogger } from '@shared/interfaces/logs';
+import { Logger } from '@shared/utils/logger/logger';
 
 export class UpdatePost {
+    private readonly logger: ILogger = new Logger(UpdatePost.name);
+
     constructor(
         private readonly postRepository: PostRepository,
         private readonly userRepository: UserRepository,
@@ -14,44 +18,51 @@ export class UpdatePost {
     ) {}
 
     async execute(id: string, updatePostDto: UpdatePostDto): Promise<Post> {
-        let post = await this.postRepository.findOne(id);
+        try {
+            let post = await this.postRepository.findOne(id);
 
-        if (!post) {
-            throw new Error(`post with id ${id} not found`);
-        }
+            if (!post) {
+                throw new Error(`post with id ${id} not found`);
+            }
 
-        if (updatePostDto.userId) {
-            post.user = await this.userRepository.findOne(updatePostDto.userId);
+            if (updatePostDto.userId) {
+                post.user = await this.userRepository.findOne(
+                    updatePostDto.userId
+                );
 
-            if (!post.user) {
-                throw new Error(
-                    `user with id ${updatePostDto.userId} not found`
+                if (!post.user) {
+                    throw new Error(
+                        `user with id ${updatePostDto.userId} not found`
+                    );
+                }
+            }
+
+            if (updatePostDto.tagsId) {
+                post.tags = await this.tagRepository.findManyByIds(
+                    updatePostDto.tagsId
                 );
             }
+
+            if (updatePostDto.commentsId) {
+                post.comments = await this.commentRepository.findManyByIds(
+                    updatePostDto.commentsId
+                );
+            }
+
+            post = await this.postRepository.update(id, {
+                ...post,
+                ...updatePostDto,
+                comments: post.comments,
+                tags: post.tags,
+                user: post.user
+            });
+
+            const serializePost = Post.serializePost(post);
+            this.logger.info(`post with id ${id} was updated`);
+
+            return serializePost;
+        } catch (error) {
+            throw new Error(`${error}`);
         }
-
-        if (updatePostDto.tagsId) {
-            post.tags = await this.tagRepository.findManyByIds(
-                updatePostDto.tagsId
-            );
-        }
-
-        if (updatePostDto.commentsId) {
-            post.comments = await this.commentRepository.findManyByIds(
-                updatePostDto.commentsId
-            );
-        }
-
-        post = await this.postRepository.update(id, {
-            ...post,
-            ...updatePostDto,
-            comments: post.comments,
-            tags: post.tags,
-            user: post.user
-        });
-
-        const serializePost = Post.serializePost(post);
-
-        return serializePost;
     }
 }
