3da0e4905c53f99e7414c8afe870e5d4d3655033
franklin rodriguez <franklinserif@gmail.com>
2025-02-16 20:36:23 -0400

    refactor(useCases/getUserById): :recycle: improve implementation

--- src/domain/useCases/users/getUserById.ts
+++ src/domain/useCases/users/getUserById.ts
@@ -1,12 +1,24 @@
 import { User } from '@domain/entities/user';
 import { UserRepository } from '@domain/repositories/user.repository';
+import { ILogger } from '@shared/interfaces/logs';
+import { Logger } from '@shared/utils/logger/logger';
 
 export class GetUserById {
+    private readonly logger: ILogger = new Logger(GetUserById.name);
+
     constructor(private userRepository: UserRepository) {}
 
     async execute(id: string): Promise<User> {
-        const user = User.serializeUser(await this.userRepository.findOne(id));
+        try {
+            const user = User.serializeUser(
+                await this.userRepository.findOne(id)
+            );
 
-        return user;
+            return user;
+        } catch (error) {
+            throw new Error(
+                `something wrong happend when tried to get list of users ${error}`
+            );
+        }
     }
 }
