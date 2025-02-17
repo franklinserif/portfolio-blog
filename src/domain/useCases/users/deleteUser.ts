3d3bb46b5bcc2913c88dfa3902bca8b446dc6719
franklin rodriguez <franklinserif@gmail.com>
2025-02-16 20:36:48 -0400

    refactor(useCases/deleteUser): :recycle: improve implementation

--- src/domain/useCases/users/deleteUser.ts
+++ src/domain/useCases/users/deleteUser.ts
@@ -1,15 +1,27 @@
 import { UserRepository } from '@domain/repositories/user.repository';
+import { ILogger } from '@shared/interfaces/logs';
+import { Logger } from '@shared/utils/logger/logger';
 
 export class DeleteUser {
+    private readonly logger: ILogger = new Logger(DeleteUser.name);
+
     constructor(private userRepository: UserRepository) {}
 
     async execute(id: string): Promise<void> {
-        const user = await this.userRepository.findOne(id);
+        try {
+            const user = await this.userRepository.findOne(id);
 
-        if (!user) {
-            throw new Error(`User with id ${id} not found`);
+            if (!user) {
+                throw new Error(`User with id ${id} not found`);
+            }
+
+            this.logger.info(`user with id ${id} was deleted`);
+
+            await this.userRepository.remove(id);
+        } catch (error) {
+            throw new Error(
+                `something wrong happend when tried to delete an user error: ${error}`
+            );
         }
-
-        await this.userRepository.remove(id);
     }
 }
