f83c867183e97fa53dfd5c81265e9c7ad2f642d1
franklin rodriguez <franklinserif@gmail.com>
2025-02-28 12:31:04 -0400

    docs(post.controller): :memo: add comments

--- src/presentation/controllers/post.controller.ts
+++ src/presentation/controllers/post.controller.ts
@@ -12,6 +12,12 @@ export class PostController {
         this.postService = postService;
     }
 
+    /**
+     * Handles the request to find a post by its ID.
+     * @param {Request} req - The Express request object.
+     * @param {Response} res - The Express response object.
+     * @returns {Promise<void>} - A promise that resolves when the response is sent.
+     */
     async findPost(req: Request, res: Response): Promise<void> {
         const { id } = req.params as { id: string };
 
@@ -20,12 +26,24 @@ export class PostController {
         res.status(200).json(post);
     }
 
+    /**
+     * Handles the request to find all posts.
+     * @param {Request} _ - The Express request object (unused).
+     * @param {Response} res - The Express response object.
+     * @returns {Promise<void>} - A promise that resolves when the response is sent.
+     */
     async findAllPosts(_: Request, res: Response): Promise<void> {
         const posts = await this.postService.findAll();
 
         res.status(200).json(posts);
     }
 
+    /**
+     * Handles the request to create a new post.
+     * @param {Request} req - The Express request object containing the post data.
+     * @param {Response} res - The Express response object.
+     * @returns {Promise<void>} - A promise that resolves when the response is sent.
+     */
     async createPost(req: Request, res: Response): Promise<void> {
         const createPostDto: CreatePostDto = req.body;
 
@@ -34,6 +52,12 @@ export class PostController {
         res.status(200).json(post);
     }
 
+    /**
+     * Handles the request to update an existing post.
+     * @param {Request} req - The Express request object containing the post ID and updated data.
+     * @param {Response} res - The Express response object.
+     * @returns {Promise<void>} - A promise that resolves when the response is sent.
+     */
     async updatePost(req: Request, res: Response): Promise<void> {
         const { id } = req.params as { id: string };
         const updatePostDto: UpdatePostDto = req.body;
@@ -43,6 +67,12 @@ export class PostController {
         res.status(200).json(post);
     }
 
+    /**
+     * Handles the request to delete a post by its ID.
+     * @param {Request} req - The Express request object containing the post ID.
+     * @param {Response} res - The Express response object.
+     * @returns {Promise<void>} - A promise that resolves when the response is sent.
+     */
     async removePost(req: Request, res: Response): Promise<void> {
         const { id } = req.params as { id: string };
 
