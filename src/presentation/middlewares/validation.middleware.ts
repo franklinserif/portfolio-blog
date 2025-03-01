bddd12ba2ad68004700e7ba48a1d9a5f1d8823e1
franklin rodriguez <franklinserif@gmail.com>
2025-03-01 00:56:53 -0400

    refactor(validation.middleware): :recycle: add a second parameter 'data' to specify the data source params or body

--- src/presentation/middlewares/validation.middleware.ts
+++ src/presentation/middlewares/validation.middleware.ts
@@ -3,14 +3,15 @@ import { ClassConstructor, plainToInstance } from 'class-transformer';
 import { validate } from 'class-validator';
 
 export function validationMiddleware<T extends object>(
-    type: ClassConstructor<T>
+    type: ClassConstructor<T>,
+    data: 'body' | 'params' = 'body'
 ): (req: Request, res: Response, next: NextFunction) => Promise<void> {
     return async (
         req: Request,
         res: Response,
         next: NextFunction
     ): Promise<void> => {
-        const errors = await validate(plainToInstance(type, req.body));
+        const errors = await validate(plainToInstance(type, req[data]));
         if (errors.length > 0) {
             res.status(400).json(errors);
         } else {
