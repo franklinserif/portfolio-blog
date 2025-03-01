b662e88b34e7b751d7e76216890e03f5f4fa34c0
franklin rodriguez <franklinserif@gmail.com>
2025-03-01 00:58:42 -0400

    feat(comment.router): add validation middleware

--- src/presentation/routes/comment.router.ts
+++ src/presentation/routes/comment.router.ts
@@ -1,6 +1,11 @@
 import { autoInjectable } from 'tsyringe';
 import { BaseRouter } from '@presentation/routes/router';
 import { CommentController } from '@presentation/controllers/comment.controller';
+import { CreateCommentDto } from '@application/dtos/comments/createComment';
+import { validationMiddleware } from '@presentation/middlewares/validation.middleware';
+import { UpdateCommentDto } from '@application/dtos/comments/updateComment';
+import { IdParamDto } from '@shared/dtos/idParam.dto';
+import { REQUEST } from '@shared/enums/data';
 
 @autoInjectable()
 export class CommentRouter extends BaseRouter {
@@ -16,20 +21,29 @@ export class CommentRouter extends BaseRouter {
             this.commentController.findAllComment(req, res)
         );
 
-        this.router.get('/comments/:id', (req, res) =>
-            this.commentController.findComment(req, res)
+        this.router.get(
+            '/comments/:id',
+            validationMiddleware(IdParamDto, REQUEST.PARAMS),
+            (req, res) => this.commentController.findComment(req, res)
         );
 
-        this.router.post('/comments', (req, res) =>
-            this.commentController.createComment(req, res)
+        this.router.post(
+            '/comments',
+            validationMiddleware(CreateCommentDto),
+            (req, res) => this.commentController.createComment(req, res)
         );
 
-        this.router.put('/comments/:id', (req, res) =>
-            this.commentController.updateComment(req, res)
+        this.router.put(
+            '/comments/:id',
+            validationMiddleware(IdParamDto, REQUEST.PARAMS),
+            validationMiddleware(UpdateCommentDto),
+            (req, res) => this.commentController.updateComment(req, res)
         );
 
-        this.router.delete('/comments/:id', (req, res) =>
-            this.commentController.removeComment(req, res)
+        this.router.delete(
+            '/comments/:id',
+            validationMiddleware(IdParamDto, REQUEST.PARAMS),
+            (req, res) => this.commentController.removeComment(req, res)
         );
     }
 }
